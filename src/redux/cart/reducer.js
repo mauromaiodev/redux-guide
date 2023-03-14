53621756ee23ec7911622c11229563b8bee11854
Mauro <mauromaio.dev@gmail.com>
2023-03-14 14:09:30 -0300

    added-new-features-in-cartReducer

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -8,9 +8,27 @@ const initialState = {
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case CartActionTypes.ADD_PRODUCT:
+      // verificar se o produto já está no carrinho
+      const productIsAlreadyInCart = state.products.some(
+        (product) => product.id === action.payload.id
+      );
+
+      // se ele estiver, aumentar a sua quantidade em 1
+      if (productIsAlreadyInCart) {
+        return {
+          ...state,
+          products: state.products.map((product) =>
+            product.id === action.payload.id
+              ? { ...product, quantity: product.quantity + 1 }
+              : product
+          ),
+        };
+      }
+
+      // se ele não estiver, adiciona-lo
       return {
         ...state,
-        products: [...state.products, action.payload],
+        products: [...state.products, { ...action.payload, quantity: 1 }],
       };
     default:
       return state;
